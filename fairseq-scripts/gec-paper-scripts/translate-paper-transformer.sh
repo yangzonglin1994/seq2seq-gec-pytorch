08db413ec99e14ad14f0e7a28e27b146c0fdba9d
yangzonglin <806765278@qq.com>
2018-11-20 20:49:31 +0800

    Add scripts for transformer model using `gec-paper`.

---
+++ fairseq-scripts/gec-paper-scripts/translate-paper-transformer.sh
@@ -0,0 +1,8 @@
+PROJECT_ROOT='../..'
+MODEL_DIR=$PROJECT_ROOT/checkpoints/gec-paper/transformer
+
+python $PROJECT_ROOT/fairseq/generate.py \
+    $PROJECT_ROOT/data-bin/gec-paper/ \
+    --path $MODEL_DIR/checkpoint_best.pt \
+    --batch-size 128 \
+    --beam 5
